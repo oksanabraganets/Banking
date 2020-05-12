81326ae83e7c4106bec862ff1ea6860487728473
oksana <oksanaov@gmail.com>
2020-05-12 12:11:03 +0300

    test for interest calculation for deposit account added

--- src/test/java/WhenCalculateInterest.java
+++ src/test/java/WhenCalculateInterest.java
@@ -1,5 +1,7 @@
 import org.example.model.entity.CreditAccount;
 import org.example.model.entity.CreditBuilder;
+import org.example.model.entity.DepositAccount;
+import org.example.model.entity.DepositBuilder;
 import org.example.model.service.Calculator;
 
 
@@ -16,7 +18,18 @@ public class WhenCalculateInterest {
                 .build();
         Calculator calculator = new Calculator();
         calculator.calculateCreditInterest(account);
-        assertEquals(450, account.getAccrued());
-        assertEquals(1450, account.getDebt());
+        assertEquals(37, account.getAccrued());
+        assertEquals(1037, account.getDebt());
+    }
+    @org.junit.Test
+    public void testCalculateDepositInterest(){
+        DepositAccount account = new DepositBuilder()
+                .balance(0)
+                .depositAmount(100000)
+                .depositRate(5)
+                .build();
+        System.out.println(5000/12);
+        new Calculator().calculateDepositInterest(account);
+        assertEquals(416, account.getBalance());
     }
 }
