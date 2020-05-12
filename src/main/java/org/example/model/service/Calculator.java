981cf05d30631808250618f41aa16440201dafb8
oksana <oksanaov@gmail.com>
2020-05-12 12:10:10 +0300

    interest calculation for deposit account imlemented

--- src/main/java/org/example/model/service/Calculator.java
+++ src/main/java/org/example/model/service/Calculator.java
@@ -1,12 +1,21 @@
 package org.example.model.service;
 
 import org.example.model.entity.CreditAccount;
+import org.example.model.entity.DepositAccount;
 
 public class Calculator {
+
     private static final int HUNDRED_PERCENTS = 100;
+    private static final int NUMBER_MONTHS = 12;
+
     public void calculateCreditInterest(CreditAccount account){
-        int accruedInterest = (-account.getBalance() * account.getCreditRate()) / HUNDRED_PERCENTS;
+        int accruedInterest = (-account.getBalance() * account.getCreditRate()) / HUNDRED_PERCENTS / NUMBER_MONTHS;
         account.setAccrued(accruedInterest);
         account.setDebt(account.getDebt() + accruedInterest);
+    }
+
+    public void calculateDepositInterest(DepositAccount account){
+        int accruedInterest = account.getDepositAmount() * account.getDepositRate() / HUNDRED_PERCENTS / NUMBER_MONTHS;
+        account.setBalance(account.getBalance() + accruedInterest);
     }
 }
