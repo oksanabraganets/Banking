ab184bbb65218968b699d8fa8894c5ff6ad50b1a
oksana <oksanaov@gmail.com>
2020-05-21 00:07:07 +0300

    user accounts removed from request attributes

--- src/main/java/org/example/controller/command/Login.java
+++ src/main/java/org/example/controller/command/Login.java
@@ -23,31 +23,21 @@ public class Login implements Command {
         if( name == null || name.equals("") || pass == null || pass.equals("")  ){
             return "/login.jsp";
         }
-
         if(CommandUtility.checkUserIsLogged(request, name)){
             return "/WEB-INF/error.jsp";
         }
-
         User user = userService.getUserByEmailPassword(name, CommandUtility.hashPassword(pass));
-        List<Account> accounts = user.getAccounts();
-        System.out.println("User accounts : " + accounts);
-
         if( user == null ) return "/WEB-INF/error.jsp";
-
         User.ROLE role = user.getRole();
-
         if (user.getRole().equals(User.ROLE.ROLE_ADMIN)){
             CommandUtility.setUserRole(request, User.ROLE.ROLE_ADMIN, user);
             return "/WEB-INF/admin/adminbasis.jsp";
         } else if(user.getRole().equals(User.ROLE.ROLE_USER)) {
             CommandUtility.setUserRole(request, User.ROLE.ROLE_USER, user);
-            request.setAttribute("accounts" , accounts);
-
             return "/WEB-INF/user/userbasis.jsp";
         } else {
             CommandUtility.setUserRole(request, User.ROLE.ROLE_UNKNOWN, user);
             return "/login.jsp";
         }
-
     }
 }
