4983b9ee83d2edc54ed2111a6149b7b510a505bd
oksana <oksanaov@gmail.com>
2020-05-21 00:04:25 +0300

    Method to get localized user name

--- src/main/java/org/example/controller/command/CommandUtility.java
+++ src/main/java/org/example/controller/command/CommandUtility.java
@@ -7,14 +7,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.security.MessageDigest;
 import java.util.HashSet;
+import java.util.Locale;
 
 public class CommandUtility {
 
     static void setUserRole(HttpServletRequest request,
                             User.ROLE role, User user) {
+
         HttpSession session = request.getSession();
         session.setAttribute("user", user);
         session.setAttribute("role", role);
+        request.setAttribute("userName", getCurrentUserName(session));
+    }
+
+    public static String getCurrentUserName(HttpSession session){
+        Locale locale = (Locale) session.getAttribute("locale");
+        User user = (User) session.getAttribute("user");
+
+        if (user == null) return null;
+        if (locale.toString().equals("uk")){
+            return user.getFirstNameUkr() + " " + user.getLastNameUkr();
+        }
+        return user.getFirstName() + " " + user.getLastName();
     }
 
     static boolean checkUserIsLogged(HttpServletRequest request, String userName){
@@ -35,10 +49,12 @@ public class CommandUtility {
     public static void removeLoggedUser(HttpSession session){
         HashSet<String> loggedUsers = (HashSet<String>) session.getServletContext()
                 .getAttribute("loggedUsers");
-        String userName = (String) session
-                .getAttribute("userName");
-        loggedUsers.remove(userName);
-        session.setAttribute("loggedUsers", loggedUsers);
+        if (loggedUsers != null) {
+            String userName = (String) session
+                    .getAttribute("userName");
+            loggedUsers.remove(userName);
+            session.setAttribute("loggedUsers", loggedUsers);
+        }
     }
 
     public static String hashPassword(String password){
