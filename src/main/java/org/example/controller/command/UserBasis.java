a966f08b9845f5f3a51d5a6c58b26eaadaeba92f
oksana <oksanaov@gmail.com>
2020-05-23 14:28:37 +0300

    UserBasis now gets the actual data from DB

--- src/main/java/org/example/controller/command/UserBasis.java
+++ src/main/java/org/example/controller/command/UserBasis.java
@@ -1,10 +1,25 @@
 package org.example.controller.command;
 
+import org.example.model.entity.User;
+import org.example.model.service.UserService;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 public class UserBasis implements Command {
+    UserService userService;
+
+    public UserBasis(UserService userService) {
+        this.userService = userService;
+    }
 
     public String execute(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        User updatedUser = userService.getUserByEmailPassword(
+                user.getEmail(),
+                user.getPassword());
+        session.setAttribute("user", updatedUser);
         return "/WEB-INF/user/userbasis.jsp";
     }
 }
