dbb0d477868fb3bb37d8a8b6b10f9a55701181e3
oksana <oksanaov@gmail.com>
2020-05-25 09:15:35 +0300

    getter for connection added

--- src/main/java/org/example/model/dao/AccountDao.java
+++ src/main/java/org/example/model/dao/AccountDao.java
@@ -2,8 +2,11 @@ package org.example.model.dao;
 
 import org.example.model.entity.Account;
 
+import java.sql.Connection;
 import java.util.List;
 
 public interface AccountDao extends GenericDao<Account> {
+
+    public Connection getConnection();
 
 }
