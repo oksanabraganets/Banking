0e243378b11c3aadb72acad93709f2efa947d011
oksana <oksanaov@gmail.com>
2020-05-25 09:27:13 +0300

    initial account created and saved to DB with user

--- src/main/java/org/example/model/service/RegistrationService.java
+++ src/main/java/org/example/model/service/RegistrationService.java
@@ -1,16 +1,21 @@
 package org.example.model.service;
 
+import org.example.model.dao.AccountDao;
 import org.example.model.dao.DaoFactory;
 import org.example.model.dao.UserDao;
+import org.example.model.entity.Account;
 import org.example.model.entity.User;
 
 public class RegistrationService {
     DaoFactory daoFactory = DaoFactory.getInstance();
 
-    public boolean saveUser(User entity){
+    public void saveUser(User entity){
         System.out.println("In service: " + entity);
         try (UserDao dao = daoFactory.createUserDao()) {
-            return dao.saveUser(entity);
+            dao.saveUser(entity);
+        }
+        try (AccountDao dao = daoFactory.createAccountDao()){
+            entity.getAccounts().forEach(dao::create);
         }
     }
 }
