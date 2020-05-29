c3e78b9966dc5d3bd67f6edf71d9f4180f048443
oksana <oksanaov@gmail.com>
2020-05-29 21:32:29 +0300

    Interface for regex created

---
+++ src/main/java/org/example/controller/command/GlobalConstants.java
@@ -0,0 +1,7 @@
+package org.example.controller.command;
+
+public interface GlobalConstants {
+    String NAME_REGEX = "[A-Z][a-z]{1,20}";
+    String EMAIL_REGEX =  "[A-Za-z_.]{1,20}@[A-Za-z.]{1,20}";
+    String NAME_UKR = "[А-ЗЙ-ЩЮЯЇІЄҐ][а-щьюяїієґ']{1,20}";
+}
